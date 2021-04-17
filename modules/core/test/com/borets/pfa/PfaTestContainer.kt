d83b08bcfbe8431df3c4ade3d9d9b1f1eba4ecc5
sergeev_ms <sergeevms.33@gmail.com>
2021-04-17 20:10:00 +0300

    Initial.

---
+++ modules/core/test/com/borets/pfa/PfaTestContainer.kt
@@ -0,0 +1,51 @@
+package com.borets.pfa
+
+import com.haulmont.cuba.testsupport.TestContainer
+import org.junit.jupiter.api.extension.ExtensionContext
+
+open class PfaTestContainer : TestContainer() {
+
+    init {
+        appComponents = mutableListOf(
+                // list add-ons here:
+                // "com.haulmont.reports",
+                // "com.haulmont.addon.bproc",
+                "com.haulmont.cuba"
+        )
+        appPropertiesFiles = mutableListOf(
+                // List the files defined in your web.xml
+                // in appPropertiesConfig context parameter of the core module
+                "com/borets/pfa/app.properties",
+                // Add this file which is located in CUBA and defines some properties
+                // specifically for test environment. You can replace it with your own
+                // or add another one in the end.
+                "com/borets/pfa/test-app.properties")
+
+        this.autoConfigureDataSource()
+    }
+
+    class Common private constructor() : PfaTestContainer() {
+
+        @Throws(Throwable::class)
+        override fun beforeAll(extensionContext: ExtensionContext) {
+            if (!initialized) {
+                super.beforeAll(extensionContext)
+                initialized = true
+            }
+            setupContext()
+        }
+
+        override fun afterAll(extensionContext: ExtensionContext) {
+            cleanupContext()
+            // never stops - do not call super
+        }
+
+        companion object {
+
+            val INSTANCE = Common()
+
+            @Volatile
+            private var initialized: Boolean = false
+        }
+    }
+}
