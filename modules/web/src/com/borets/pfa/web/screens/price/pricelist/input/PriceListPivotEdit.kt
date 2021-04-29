b1ea5064d715429adbbe8a715a52242927a175f3
sergeev_ms <sergeevms.33@gmail.com>
2021-04-29 12:28:54 +0300

    priceList now use pivotInitializer bean

--- modules/web/src/com/borets/pfa/web/screens/price/pricelist/input/PriceListPivotEdit.kt
+++ modules/web/src/com/borets/pfa/web/screens/price/pricelist/input/PriceListPivotEdit.kt
@@ -5,19 +5,24 @@ import com.borets.pfa.entity.activity.JobType
 import com.borets.pfa.entity.activity.WellEquip
 import com.borets.pfa.entity.activity.WellTag
 import com.borets.pfa.entity.analytic.AnalyticSet
-import com.haulmont.cuba.gui.screen.*
 import com.borets.pfa.entity.price.PriceList
 import com.borets.pfa.entity.price.PriceListDetail
 import com.borets.pfa.entity.price.RevenueType
+import com.borets.pfa.web.beans.PivotGridInitializer
 import com.haulmont.cuba.core.entity.KeyValueEntity
+import com.haulmont.cuba.core.global.AppBeans
 import com.haulmont.cuba.core.global.DataManager
 import com.haulmont.cuba.core.global.Messages
-import com.haulmont.cuba.gui.UiComponents
-import com.haulmont.cuba.gui.components.*
-import com.haulmont.cuba.gui.model.*
-import com.haulmont.cuba.gui.screen.Target
-import com.vaadin.ui.themes.ValoTheme
+import com.haulmont.cuba.gui.components.CurrencyField
+import com.haulmont.cuba.gui.components.GridLayout
+import com.haulmont.cuba.gui.components.LookupField
+import com.haulmont.cuba.gui.components.TextField
+import com.haulmont.cuba.gui.model.CollectionPropertyContainer
+import com.haulmont.cuba.gui.model.DataContext
+import com.haulmont.cuba.gui.model.KeyValueCollectionContainer
+import com.haulmont.cuba.gui.screen.*
 import java.math.BigDecimal
+import java.util.*
 import javax.inject.Inject
 
 @UiController("pfa_PriceListPivot.edit")
@@ -25,10 +30,9 @@ import javax.inject.Inject
 @EditedEntityContainer("priceListDc")
 @LoadDataBeforeShow
 class PriceListPivotEdit : StandardEditor<PriceList>() {
+
     @Inject
     private lateinit var dataManager: DataManager
-    @Inject
-    private lateinit var uiComponents: UiComponents
     @Inject
     private lateinit var messages: Messages
     @Inject
@@ -36,8 +40,6 @@ class PriceListPivotEdit : StandardEditor<PriceList>() {
 
     @Inject
     private lateinit var detailsDc: CollectionPropertyContainer<PriceListDetail>
-    @Inject
-    private lateinit var detailsPivotDc: KeyValueCollectionContainer
 
     @Inject
     private lateinit var pivotGrid: GridLayout
@@ -50,185 +52,101 @@ class PriceListPivotEdit : StandardEditor<PriceList>() {
     @Inject
     private lateinit var filterWellTagField: LookupField<WellTag>
 
-
-    private var pivotStaticProperties = mapOf(
-        Pair("contractType", ContractType::class), Pair("jobType", ContractType::class),
-        Pair("wellEquip", WellEquip::class), Pair("wellTag", WellTag::class))
-
-    private var pivotGridRows = 1 //cause we have to skip header row
+    private lateinit var pivotGridHelper: PivotGridInitializer
 
 
     @Subscribe
     private fun onAfterInit(event: AfterInitEvent) {
+        pivotGridHelper = AppBeans.getPrototype(PivotGridInitializer::class.java, pivotGrid)
         initFilter()
     }
 
     @Subscribe
     private fun onBeforeShow(event: BeforeShowEvent) {
-        initStaticPivotProperties()
-        initStaticPivotPropertiesValue()
+        val pivotStaticProperties = listOf(
+            PivotGridInitializer.StaticPropertyData("analytic", "", true, AnalyticSet::class.java, null, null, false),
+
+            PivotGridInitializer.StaticPropertyData(
+                "contractType", messages.getMessage(AnalyticSet::class.java, "AnalyticSet.contractType"),
+                false, ContractType::class.java, null, null, true
+            ),
+            PivotGridInitializer.StaticPropertyData(
+                "jobType", messages.getMessage(AnalyticSet::class.java, "AnalyticSet.jobType"),
+                true, JobType::class.java, null, null, true
+            ),
+            PivotGridInitializer.StaticPropertyData(
+                "wellEquip", messages.getMessage(AnalyticSet::class.java, "AnalyticSet.wellEquip"),
+                true, WellEquip::class.java, null, null, true
+            ),
+            PivotGridInitializer.StaticPropertyData(
+                "wellTag", messages.getMessage(AnalyticSet::class.java, "AnalyticSet.wellTag"),
+                true, WellTag::class.java, null, null, true
+            )
+        )
+
+
+
+        pivotGridHelper.initStaticPivotProperties(pivotStaticProperties)
+
+        pivotGridHelper.setStaticPivotPropertiesValues(initKvEntities())
+
+        pivotGridHelper.setStoreFunction() { key : Any, property : String, value : Any? ->
+            var detail = detailsDc.mutableItems.find { it.analytic == key && property == it.revenueType!!.id.toString() }
+            if (detail == null) {
+                detail = dataContext.create(PriceListDetail::class.java).apply {
+                    this.priceList = editedEntity
+                    this.analytic = key as AnalyticSet
+                    this.revenueType = dataManager.getReference(RevenueType::class.java, UUID.fromString(property))
+                }
+            }
+            detail!!.value = value as BigDecimal?
+        }
     }
 
     @Subscribe
     private fun onAfterShow(event: AfterShowEvent) {
-        initDynamicPivotProperties()
+        initDynamic()
     }
 
-
-    private fun initStaticPivotProperties() {
-        initStaticColumnsCaptions()
-        pivotStaticProperties.forEach { (property, clazz) ->
-            detailsPivotDc.addProperty(property, clazz.java)
-        }
-        //fixme: hardcoded
-        detailsPivotDc.addProperty("analytic", AnalyticSet::class.java)
-    }
-
-    private fun initDynamicPivotProperties() {
-        dataManager.load(RevenueType::class.java)
-            .list()
-            .toList()
-            .let { revenueTypes ->
-                initDynamicColumnsCaptions(revenueTypes.map { it.name!! })
-                initDynamicDcPivotProperties(revenueTypes)
-                initDynamicPivotPropertiesFields(revenueTypes)
+    private fun initDynamic() {
+        dataManager.load(RevenueType::class.java).list()
+            .map { PivotGridInitializer.DynamicPropertyData(
+                it.id.toString(),
+                it.name!!,
+                BigDecimal::class.javaObjectType, CurrencyField::class.java, "70px")
             }
-    }
-
-    private fun initDynamicDcPivotProperties(revenueTypes: List<RevenueType>) {
-        revenueTypes.forEach {
-            detailsPivotDc.addProperty(it.name!!, BigDecimal::class.java)
-
-            initDynamicPropertiesValues(it)
-        }
-    }
-
-    private fun initDynamicPropertiesValues(revenueType: RevenueType) {
-        detailsDc.items.forEach { detail ->
-            if (detail.value != null) {
-                detailsPivotDc.items
-                    .find { detail.analytic == it.getValue<AnalyticSet>("analytic")
-                            && detail.revenueType == revenueType}
-                    ?.setValue(revenueType.name!!, detail.value)
-            }
-        }
-    }
-
-    private fun initStaticColumnsCaptions() {
-        pivotGrid.columns = pivotStaticProperties.size
-        var colsStartPosition = 0
-        pivotStaticProperties.forEach { (property, _) ->
-            val caption = messages.getMessage(AnalyticSet::class.java, "AnalyticSet.${property}")
-            addColumnCaption(caption, colsStartPosition++)
-        }
-    }
-
-    private fun initDynamicColumnsCaptions(captions: List<String>){
-        var startCol = pivotGrid.columns
-        pivotGrid.columns += captions.size
-        captions.forEach {
-            addColumnCaption(it, startCol++)
-        }
-    }
-
-    private fun addColumnCaption(caption: String, startPosition: Int) {
-        @Suppress("UnstableApiUsage")
-        uiComponents.create(Label.TYPE_STRING).apply {
-            this.value = caption
-            this.addStyleName(ValoTheme.LABEL_H3)
-            this.alignment = Component.Alignment.MIDDLE_CENTER
-        }.let {
-            pivotGrid.add(it, startPosition, 0)
-        }
-    }
-
-    private fun initStaticPivotPropertiesValue() {
-        dataManager.load(AnalyticSet::class.java)
-            .list()
-            .forEach {
-                detailsPivotDc.mutableItems.add(
-                    KeyValueEntity().apply {
-                        this.setValue("contractType", it.getContractType())
-                        this.setValue("jobType", it.getJobType())
-                        this.setValue("wellEquip", it.getWellEquip())
-                        this.setValue("wellTag", it.getWellTag())
-                        //fixme: hardcoded
-                        this.setValue("analytic", it)
+            .let { dynamicProperties ->
+                pivotGridHelper.initDynamicProperties(dynamicProperties)
+                pivotGridHelper.setDynamicPropertiesValues { kvDc : KeyValueCollectionContainer ->
+                    detailsDc.items.forEach { detail ->
+                        detail.value?.let { value ->
+                            kvDc.items
+                                .find { detail.analytic == it.getValue<AnalyticSet>("analytic") }
+                                ?.setValue(detail.revenueType!!.id.toString(), value)
+                        }
                     }
-                )
-            }
-    }
-
-    private fun initDynamicPivotPropertiesFields(revenueTypes: List<RevenueType>) {
-        val skipRows = 1 //avoid captions
-        val skipColumns = pivotStaticProperties.size //avoid static fields
-        detailsPivotDc.items.forEachIndexed { rowIndex, keyValueEntity ->
-            revenueTypes.forEachIndexed { colIndex, revenueType ->
-                @Suppress("UnstableApiUsage")
-                uiComponents.create(CurrencyField.TYPE_BIGDECIMAL).apply {
-                    this.value = keyValueEntity.getValue(revenueType.name!!)
-                    this.setWidth("85px")
-                    this.addValueChangeListener {
-                        keyValueEntity.setValue(revenueType.name!!, it.value) //set "visual" value
-                        setStoredValue(keyValueEntity.getValue("analytic")!!, revenueType, it.value) //set "real" value
-                    }
-                }.also {
-                    pivotGrid.add(it, colIndex + skipColumns, rowIndex +skipRows)
                 }
             }
-        }
     }
 
-
-
-    @Subscribe(id = "detailsPivotDc", target = Target.DATA_CONTAINER)
-    private fun onDetailsPivotDcCollectionChange(event: CollectionContainer.CollectionChangeEvent<KeyValueEntity>) {
-        when (event.changeType) {
-            CollectionChangeType.ADD_ITEMS -> {
-                addRows(event.changes)
+    private fun initKvEntities(): List<KeyValueEntity> {
+        return dataManager.load(AnalyticSet::class.java)
+            .list()
+            .map {
+                KeyValueEntity().apply {
+                    this.setValue("analytic", it)
+                    this.setValue("contractType", it.getContractType())
+                    this.setValue("jobType", it.getJobType())
+                    this.setValue("wellEquip", it.getWellEquip())
+                    this.setValue("wellTag", it.getWellTag())
+                }
             }
-            else -> {} //todo:
-        }
-    }
-
-    private fun addRows(changes: Collection<KeyValueEntity>) {
-        pivotGrid.rows = pivotGrid.rows + changes.size
-        changes.forEach {
-            addColumns(it)
-            pivotGridRows++
-        }
-    }
-
-    private fun addColumns(keyValueEntity: KeyValueEntity) {
-        var cols = 0
-        pivotStaticProperties.forEach { (property, _) ->
-            @Suppress("UnstableApiUsage")
-            uiComponents.create(Label.TYPE_STRING).apply {
-                this.alignment = Component.Alignment.MIDDLE_LEFT
-                this.value = keyValueEntity.getValue(property)
-            }.also {
-                pivotGrid.add(it, cols++, pivotGridRows)
-            }
-        }
-    }
-
-    // tried to use ItemPropertyChangeEvent<KeyValueEntity> but isn't working
-    private fun setStoredValue(analytic: AnalyticSet, revenueType: RevenueType, value: BigDecimal?) {
-        var detail = detailsDc.mutableItems.find { it.analytic == analytic && revenueType == it.revenueType }
-        if (detail == null) {
-            detail = dataContext.create(PriceListDetail::class.java).apply {
-                this.priceList = editedEntity
-                this.analytic = analytic
-                this.revenueType = revenueType
-            }
-        }
-        detail!!.value = value
     }
 
     private fun initFilter() {
         listOf(filterContractTypeField, filterJobTypeField, filterWellEquipField, filterWellTagField).forEach { field ->
             field.addValueChangeListener { _ ->
-                detailsPivotDc.items.forEachIndexed { index, keyValueEntity ->
+                pivotGridHelper.kvCollectionContainer.items.forEachIndexed { index, keyValueEntity ->
                     val isContract = filterContractTypeField.value?.let {
                         keyValueEntity.getValueEx<ContractType>("analytic.contractType") == it
                     } ?: true
@@ -242,15 +160,9 @@ class PriceListPivotEdit : StandardEditor<PriceList>() {
                         keyValueEntity.getValueEx<WellTag>("analytic.wellTag") == it
                     } ?: true
 
-                    setPivotRowVisibility(index + 1, isContract && isJobType && isWellEquip && isWellTag)
+                    pivotGridHelper.setPivotRowVisibility(index + 1, isContract && isJobType && isWellEquip && isWellTag)
                 }
             }
-        }
-    }
-
-    private fun setPivotRowVisibility(rowIndex: Int, visible: Boolean) {
-        for (colIndex in 0 until pivotGrid.columns) {
-            pivotGrid.getComponentNN(colIndex, rowIndex).isVisible = visible
         }
     }
 }
