cc3fb7a7454441c4b5da56eb74dbc91f6b6b59f4
sergeev_ms <sergeevms.33@gmail.com>
2021-05-04 17:54:34 +0300

    recreate well-tag enum

--- modules/global/src/com/borets/pfa/entity/activity/WellTag.kt
+++ modules/global/src/com/borets/pfa/entity/activity/WellTag.kt
@@ -3,8 +3,8 @@ package com.borets.pfa.entity.activity
 import com.haulmont.chile.core.datatypes.impl.EnumClass
 
 enum class WellTag(private val id: String) : EnumClass<String> {
-    NEW("N"),
-    SECOND_RUN("SR");
+    FIRST("F"),
+    SEQUENT("S");
 
     override fun getId() = id
 
