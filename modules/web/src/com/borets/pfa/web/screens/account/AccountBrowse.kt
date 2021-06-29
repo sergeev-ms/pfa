5481ba08cab249fb53cfffa834cc89ffbd7772c3
sergeev_ms <sergeevms.33@gmail.com>
2021-06-29 15:53:26 +0300

    add expandAll and collapseAll actions

--- modules/web/src/com/borets/pfa/web/screens/account/AccountBrowse.kt
+++ modules/web/src/com/borets/pfa/web/screens/account/AccountBrowse.kt
@@ -2,9 +2,25 @@ package com.borets.pfa.web.screens.account
 
 import com.haulmont.cuba.gui.screen.*
 import com.borets.pfa.entity.account.Account
+import com.haulmont.cuba.gui.components.Action
+import com.haulmont.cuba.gui.components.TreeTable
+import javax.inject.Inject
 
 @UiController("pfa_Account.browse")
 @UiDescriptor("account-browse.xml")
 @LookupComponent("accountsTable")
 @LoadDataBeforeShow
-class AccountBrowse : StandardLookup<Account>()
+class AccountBrowse : StandardLookup<Account>() {
+    @Inject
+    private lateinit var accountsTable: TreeTable<Account>
+
+    @Subscribe("accountsTable.expandAll")
+    private fun onAccountsTableExpandAll(event: Action.ActionPerformedEvent) {
+        accountsTable.expandAll()
+    }
+
+    @Subscribe("accountsTable.collapseAll")
+    private fun onAccountsTableCollapseAll(event: Action.ActionPerformedEvent) {
+        accountsTable.collapseAll()
+    }
+}
