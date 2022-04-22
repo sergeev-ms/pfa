48fd3d40346a155a9680ddfac7dc3286f5aadd1b
barser <sb@barser.ru>
2022-04-22 19:18:35 +0300

    issue borets/pfa#75 Reports enhancements
    Revenue report:
    1. Changed digits formats (no decimals, thousand separator);
    2. Subtotal for each account category (Strategic, Keys, Others);
    3. Subtotal per each month;
    4. Added Sales Manager column.

--- modules/core/src/com/borets/pfa/report/custom/Account.java
+++ modules/core/src/com/borets/pfa/report/custom/Account.java
@@ -14,12 +14,15 @@ public class Account {
 
     private final String applicationType;
 
+    private final String accountManager;
+
     private final int order;
 
-    public Account(String type, String parent, String customer, String businessModel, String applicationType, int order) {
+    public Account(String type, String parent, String customer, String accountManager, String businessModel, String applicationType, int order) {
         this.type = Objects.requireNonNull(type);
         this.parent = parent;
         this.customer = Objects.requireNonNull(customer);
+        this.accountManager = accountManager;
         this.businessModel = Objects.requireNonNull(businessModel);
         this.applicationType = Objects.requireNonNull(applicationType);
         this.order = order;
@@ -31,13 +34,13 @@ public class Account {
         if (o == null || getClass() != o.getClass()) return false;
         Account account = (Account) o;
         return order == account.order && type.equals(account.type) && Objects.equals(parent, account.parent)
-                && customer.equals(account.customer) && businessModel.equals(account.businessModel)
-                && applicationType.equals(account.applicationType);
+                && customer.equals(account.customer) && Objects.equals(accountManager, account.accountManager)
+                && businessModel.equals(account.businessModel) && applicationType.equals(account.applicationType);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(type, parent, customer, businessModel, applicationType, order);
+        return Objects.hash(type, parent, accountManager, customer, businessModel, applicationType, order);
     }
 
     public String getType() {
@@ -50,6 +53,10 @@ public class Account {
 
     public String getCustomer() {
         return customer;
+    }
+
+    public String getAccountManager() {
+        return accountManager;
     }
 
     public String getBusinessModel() {
@@ -70,6 +77,7 @@ public class Account {
                 "type='" + type + '\'' +
                 ", parent='" + parent + '\'' +
                 ", customer='" + customer + '\'' +
+                ", accountManager='" + accountManager + '\'' +
                 ", businessModel='" + businessModel + '\'' +
                 ", applicationType='" + applicationType + '\'' +
                 ", order=" + order +
